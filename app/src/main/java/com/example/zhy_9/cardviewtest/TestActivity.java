adfb148195d4034eb831be005342b91f456e6fef
zxk901021 <447360581@qq.com>
2015-04-30 10:50:53 +0800

    First commit

--- app/src/main/java/com/example/zhy_9/cardviewtest/TestActivity.java
+++ app/src/main/java/com/example/zhy_9/cardviewtest/TestActivity.java
@@ -4,14 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 
 public class TestActivity extends Activity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        btn = (Button) findViewById(R.id.btn);
+
     }
 
     @Override
